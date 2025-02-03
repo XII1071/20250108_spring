5eb49de6dc48db64217ef58f9edc387a8c0caf00
XII1071 <garmen1071@gmail.com>
2025-02-03 11:52:41 +0900

    0203

--- ex6/src/main/java/com/example/ex6/repository/MovieRepository.java
+++ ex6/src/main/java/com/example/ex6/repository/MovieRepository.java
@@ -2,8 +2,15 @@ package com.example.ex6.repository;
 
 
 import com.example.ex6.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+/* 접근 2가지. 쿼리 메서드 접근, 쿼리 어노테이션 */
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    /* Object[]를 쓴이유  복수개의 엔티티(movie, review, MovieImage)를 받기위해서 */
+    @Query("select m, avg(coalesce(r.grade, 0)) from Movie m left outer join Review r " + "left outer join")
+    Page<Object[]>getListPage(Pageable pageable);
 }
