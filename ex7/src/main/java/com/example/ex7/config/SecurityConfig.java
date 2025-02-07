411d3db40b65e6302cf6970fdc34253ea5861e5c
XII1071 <garmen1071@gmail.com>
2025-02-07 16:43:07 +0900

    0207

--- ex7/src/main/java/com/example/ex7/config/SecurityConfig.java
+++ ex7/src/main/java/com/example/ex7/config/SecurityConfig.java
@@ -10,18 +10,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
   private static final String[] AUTH_WHITELIST = {
-      "/" // "/" <- 이것만 했을때도 index로 간다.
-
+      "/" // <- 이것만 했을때도 index로 간다.
   };
 
   // spring security의 세션방식 기반으로 대부분의 설정 가능
@@ -30,12 +38,12 @@ public class SecurityConfig {
     httpSecurity.authorizeHttpRequests(new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
       @Override
       public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
-        auth.requestMatchers(AUTH_WHITELIST).permitAll() // 허용하고자 하는 주소를 String 배열로 적용
-            .requestMatchers("/sample/all/**").permitAll() // 허용하고자 하는 주소와 "**" 덧붙임
-            .requestMatchers("/sample/admin").hasRole("ADMIN") // 권한이 하나만 들어갈 때
-            .requestMatchers("/sample/manager").access(
+        auth.requestMatchers(AUTH_WHITELIST).permitAll()  //모두 수용 복수 지정때는 String배열로 적용
+            .requestMatchers("/sample/all/**").permitAll()//모두 수용 단독 지정때는 주소와 "**" 덧붙임.
+            .requestMatchers("/sample/admin").hasRole("ADMIN") //권한이 단수일 때
+            .requestMatchers("/sample/manager").access(  //권한이 복수일 때
                 new WebExpressionAuthorizationManager(
-                    "hasRole('MANAGER') or hasRole('ADMIN')"
+                    "hasRole('MANAGER') or hasRole('ADMIN') "
                 )
             )
         ;
@@ -68,6 +76,11 @@ public class SecurityConfig {
   }
 
   @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
   public AuthenticationSuccessHandler getAuthenticationSuccessHandler() {
     return new CustomLoginSuccessHandler();
   }
@@ -76,4 +89,31 @@ public class SecurityConfig {
   public AuthenticationFailureHandler getAuthenticationFailureHandler() {
     return new CustomAuthenticationFailureHandler();
   }
+
+  // InMemory 방식으로 UserDetailsService(인증 관리 객체) 사용
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails user1 = User.builder()
+        .username("user1")
+        .password("$2a$10$XGw3jOo9mQSoij4/so.6H.BtSRWpgPze6ZWMuc7ntyFFWqVNbcmBe")
+        .roles("USER")
+        .build();
+    UserDetails manager = User.builder()
+        .username("manager")
+        .password("$2a$10$AEHcuzENZx7OLeA.s8e.t.CvhE/a/GZf.ZKTPEBIKLv8g03zChnD2")
+        .roles("MANAGER")
+        .build();
+    UserDetails admin = User.builder()
+        .username("admin")
+        .password(passwordEncoder().encode("1"))
+        .roles("ADMIN", "MANAGER")
+        .build();
+    List<UserDetails> list = new ArrayList<>();
+    list.add(user1);
+    list.add(manager);
+    list.add(admin);
+    return new InMemoryUserDetailsManager(list);
+
+  }
+
 }
