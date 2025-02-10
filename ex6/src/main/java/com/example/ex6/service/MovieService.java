410060fff10f3a991975d0a77bc075b4a165edfd
XII1071 <garmen1071@gmail.com>
2025-02-10 20:23:39 +0900

    0210

--- ex6/src/main/java/com/example/ex6/service/MovieService.java
+++ ex6/src/main/java/com/example/ex6/service/MovieService.java
@@ -15,60 +15,60 @@ import java.util.stream.Collectors;
 
 public interface MovieService {
 
-  PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
+    PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
 
-  Long register(MovieDTO movieDTO);
+    Long register(MovieDTO movieDTO);
 
-  MovieDTO get(Long mno);
+    MovieDTO get(Long mno);
 
-  void removeMovieImagebyUUID(String uuid);
+    void removeMovieImagebyUUID(String uuid);
 
-  default Map<String, Object> dtoToEntity(MovieDTO movieDTO) {
-    System.out.println(movieDTO);
-    Map<String, Object> entityMap = new HashMap<>();
+    default Map<String, Object> dtoToEntity(MovieDTO movieDTO) {
+        System.out.println(movieDTO);
+        Map<String, Object> entityMap = new HashMap<>();
 
-    Movie movie = Movie.builder().mno(movieDTO.getMno()).title(movieDTO.getTitle()).build();
+        Movie movie = Movie.builder().mno(movieDTO.getMno()).title(movieDTO.getTitle()).build();
 
-    entityMap.put("movie", movie);
-    List<MovieImageDTO> imageDTOList = movieDTO.getImageDTOList();
-    if (imageDTOList != null && imageDTOList.size() > 0) {
-      List<MovieImage> movieImageList = imageDTOList.stream().map(movieImageDTO -> {
-        MovieImage movieImage = MovieImage.builder()
-            .path(movieImageDTO.getPath())
-            .imgName(movieImageDTO.getImgName())
-            .uuid(movieImageDTO.getUuid())
-            .movie(movie)
-            .build();
-        return movieImage;
-      }).collect(Collectors.toList());
-      entityMap.put("imgList", movieImageList);
+        entityMap.put("movie", movie);
+        List<MovieImageDTO> imageDTOList = movieDTO.getImageDTOList();
+        if (imageDTOList != null && imageDTOList.size() > 0) {
+            List<MovieImage> movieImageList = imageDTOList.stream().map(movieImageDTO -> {
+                MovieImage movieImage = MovieImage.builder()
+                        .path(movieImageDTO.getPath())
+                        .imgName(movieImageDTO.getImgName())
+                        .uuid(movieImageDTO.getUuid())
+                        .movie(movie)
+                        .build();
+                return movieImage;
+            }).collect(Collectors.toList());
+            entityMap.put("imgList", movieImageList);
+        }
+        return entityMap;
     }
-    return entityMap;
-  }
 
-  default MovieDTO entityToDTO(Movie movie, List<MovieImage> movieImages,
-                               Double avg, Long reviewCnt) {
-    MovieDTO movieDTO = MovieDTO.builder()
-        .mno(movie.getMno())
-        .title(movie.getTitle())
-        .regDate(movie.getRegDate())
-        .modDate(movie.getModDate())
-        .build();
-    List<MovieImageDTO> movieImageDTOList = new ArrayList<>();
-    if (movieImages.size() > 0 && movieImages.get(0) != null) {
-      movieImageDTOList = movieImages.stream().map(movieImage -> {
-        MovieImageDTO movieImageDTO = MovieImageDTO.builder()
-            .imgName(movieImage.getImgName())
-            .path(movieImage.getPath())
-            .uuid(movieImage.getUuid())
-            .build();
-        return movieImageDTO;
-      }).collect(Collectors.toList());
+    default MovieDTO entityToDTO(Movie movie, List<MovieImage> movieImages,
+                                 Double avg, Long reviewCnt) {
+        MovieDTO movieDTO = MovieDTO.builder()
+                .mno(movie.getMno())
+                .title(movie.getTitle())
+                .regDate(movie.getRegDate())
+                .modDate(movie.getModDate())
+                .build();
+        List<MovieImageDTO> movieImageDTOList = new ArrayList<>();
+        if (movieImages.size() > 0 && movieImages.get(0) != null) {
+            movieImageDTOList = movieImages.stream().map(movieImage -> {
+                MovieImageDTO movieImageDTO = MovieImageDTO.builder()
+                        .imgName(movieImage.getImgName())
+                        .path(movieImage.getPath())
+                        .uuid(movieImage.getUuid())
+                        .build();
+                return movieImageDTO;
+            }).collect(Collectors.toList());
+        }
+        movieDTO.setImageDTOList(movieImageDTOList);
+        movieDTO.setAvg(avg);
+        movieDTO.setReviewCnt(reviewCnt);
+        return movieDTO;
     }
-    movieDTO.setImageDTOList(movieImageDTOList);
-    movieDTO.setAvg(avg);
-    movieDTO.setReviewCnt(reviewCnt);
-    return movieDTO;
-  }
 
 }
